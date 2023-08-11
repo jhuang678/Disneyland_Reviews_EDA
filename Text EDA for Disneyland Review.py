f8ec838c144db86efd3b26edc980d4dcef586e69
Jeffrey Huang <100253011+jhuang678@users.noreply.github.com>
2023-08-11 12:09:00 -0700

    rename

--- Text EDA for Disneyland Review.py
+++ Text EDA for Disneyland Review.py
@@ -1,7 +1,7 @@
 ######################################################################
 import os
 import string
-from tabulate import tabulate as tab
+import tabulate as tab
 import pandas as pd
 import numpy as np
 import matplotlib
